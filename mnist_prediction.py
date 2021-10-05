e717e54f90ac41cefd5bff99e86313188f14c508
nikopoulospet <peter@nikopoulos.net>
2021-10-05 14:38:32 -0400

    dataset preprocessing

--- mnist_prediction.py
+++ mnist_prediction.py
@@ -1,6 +1,46 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation
+import matplotlib.pyplot as plt
 import numpy as np
+
+debug = True
+
+# ++++++++++ data preprocessing ++++++++++++++++++++++++++++++++++
+#load data from .npy files (static paths)
+images = np.load("images.npy") # np shape: (6500, 784) -> 784 = 28*28
+labels = np.load("labels.npy") # np shape: (6500, )
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+#set image data to 0-1 scale
+images = images / 255.0
+
+#split data into Train, Val, and test sets. (.6,.15,.25)
+shuffle = np.random.permutation(labels.shape[0])
+images = images[shuffle]
+labels = labels[shuffle]
+
+train = int(labels.shape[0] * .6)
+val = int(labels.shape[0] * .15) + train
+
+x_train = images[:train]
+y_train = labels[:train]
+x_val = images[train:val]
+y_val = labels[train:val]
+x_test = images[val:]
+y_test = labels[val:]
+
+if debug:
+    plt.figure(figsize=(10, 10))
+    for i in range(25):
+        plt.subplot(5, 5, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(x_train[i].reshape(28,28), cmap=plt.cm.binary)
+        plt.xlabel(class_names[y_train[i]])
+    plt.show()
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 # Model Template
 
