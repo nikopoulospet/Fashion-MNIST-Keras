d2a812e42bda4a3c5e304d0adda1904aba987cd3
nikopoulospet <peter@nikopoulos.net>
2021-10-05 15:06:38 -0400

    model training. Works sorta but like we gotta improve this

--- mnist_prediction.py
+++ mnist_prediction.py
@@ -1,9 +1,10 @@
+import keras.utils.np_utils
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 import matplotlib.pyplot as plt
 import numpy as np
 
-debug = True
+debug = False
 
 # ++++++++++ data preprocessing ++++++++++++++++++++++++++++++++++
 #load data from .npy files (static paths)
@@ -13,6 +14,9 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 #set image data to 0-1 scale
 images = images / 255.0
+
+#one hot encoding for labels
+labels = keras.utils.np_utils.to_categorical(labels, num_classes=10)
 
 #split data into Train, Val, and test sets. (.6,.15,.25)
 shuffle = np.random.permutation(labels.shape[0])
@@ -45,10 +49,10 @@ if debug:
 # Model Template
 
 model = Sequential() # declare model
-model.add(Dense(10, input_shape=(28*28, ), kernel_initializer='he_normal')) # first layer
+model.add(Dense(28*28, input_shape=(28*28, ), kernel_initializer='he_normal')) # first layer
 model.add(Activation('relu'))
-#
-#
+model.add(Dense(28*28, kernel_initializer='he_normal'))
+model.add(Activation('relu'))
 #
 # Fill in Model Here
 #
@@ -65,11 +69,14 @@ model.compile(optimizer='sgd',
 # Train Model
 history = model.fit(x_train, y_train, 
                     validation_data = (x_val, y_val), 
-                    epochs=10, 
+                    epochs=50,
                     batch_size=512)
 
 
 # Report Results
 
 print(history.history)
-model.predict()
+score = model.evaluate(x_test, y_test, verbose=0)
+print("Test loss:", score[0])
+print("Test accuracy:", score[1])
+# figure out confusion matrix
